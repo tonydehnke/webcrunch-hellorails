661aaba082d51ab0bdf30bc509dcfc3dec960396
Tony Dehnke <tdehnke@me.com>
2019-08-21 14:43:17 -0700

    Setup Submissions Model and remove stylesheets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_014950) do
+ActiveRecord::Schema.define(version: 2019_08_21_213331) do
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
