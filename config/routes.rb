91673c8fa3ec869e3ab279d32042af106c1930b8
Tony Dehnke <tdehnke@me.com>
2019-10-05 20:28:54 -0700

    update root to submissions index to mimic reddit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :submissions
   devise_for :users
-  root to: "home#index"
+  root to: "submissions#index"
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
